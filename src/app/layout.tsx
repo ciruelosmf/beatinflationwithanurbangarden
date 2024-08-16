4a036ef5b607f7d0665721f381783d01d94b4563
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-08-16 13:14:42 -0300

    add g analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+
+          <head>
+                <Script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+
+                <Script id="google-analytics">
+                  {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+                    page_path: window.location.pathname,                    
+          });
+                  `}
+                </Script>   
+
+          </head>
+
+
+         <body 
+                className={inter.className}>
+                                                {children}
+        </body>
     </html>
   );
 }
